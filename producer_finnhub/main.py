14568834ec93adc4c1a7f25aaa7519e1ea028d4b
Akintunde Adegbayo <Akt231@yahoo.com>
2023-10-28 20:40:23 +0100

    edit main.py file

--- producer_finnhub/main.py
+++ producer_finnhub/main.py
@@ -1,7 +1,6 @@
-import argparse
+from  argparse import ArgumentParser
 import os
 from utils.helperfnc import init_client, get_ticker
-
 
 #getting tokens from .env file
 from dotenv import load_dotenv
@@ -12,16 +11,20 @@ if __name__ == '__main__':
     #initialise finhub client
     finnhub_client = init_client(token_finnhubio)
 
-#    parser = argparse.ArgumentParser(description="Get list of tickers based on Finnhub search",
-#                                     prog="main.py",
-#                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-#    parser.add_argument('--ticker', type=str,
-#                        help="Enter the phrase to look up for a ticker")
-#
-#    args = parser.parse_args()
-#    params = vars(args)
-#
-#    try:
-#        print(lookup_ticker(finnhub_client,params['ticker']))
-#    except Exception as e:
-#        print(str(e))
+    parser = ArgumentParser(prog="main.py",
+                            usage = None,
+                            description="List of Tickers from Finnhub search",
+                            formatter_class=<class "argparse.ArgumentDefaultsHelpFormatter">,
+                             conflict_handler = "error",
+                            add_help = True
+                            )
+    
+    parser.add_argument('--ticker', type=str, help="Please supply Ticker name: Enter the phrase to look up for a ticker")
+
+    args = parser.parse_args()
+    params = vars(args)
+
+    try:
+        print(get_ticker(finnhub_client,params['ticker']))
+    except Exception as e:
+        print(str(e))
