f4116aa01cab4b7e25089a2d8e74e789389fa55e
Akintunde Adegbayo <Akt231@yahoo.com>
2023-10-28 19:38:55 +0100

    updated finnhub code

---
+++ producer_finnhub/main.py
@@ -0,0 +1,27 @@
+import argparse
+import os
+from utils.helperfnc import init_client, get_ticker
+
+from dotenv import load_dotenv
+load_dotenv()
+token_finnhubio = os.getenv('token_finnhubio')
+
+if __name__ == '__main__':
+    #initialise finhub client
+    #finnhub_client = init_client('') #uncomment this, insert token and comment following line to run locally
+    #finnhub_client = init_client(os.environ['FINNHUB_API_TOKEN'])
+    finnhub_client = init_client(token_finnhubio) #uncomment this, insert token and comment following line to run locally
+
+#    parser = argparse.ArgumentParser(description="Get list of tickers based on Finnhub search",
+#                                     prog="main.py",
+#                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+#    parser.add_argument('--ticker', type=str,
+#                        help="Enter the phrase to look up for a ticker")
+#
+#    args = parser.parse_args()
+#    params = vars(args)
+#
+#    try:
+#        print(lookup_ticker(finnhub_client,params['ticker']))
+#    except Exception as e:
+#        print(str(e))
