1dab5f07201bdfea8c656e4736f31b73b54acdff
Akintunde Adegbayo <Akt231@yahoo.com>
2023-11-01 00:32:38 +0100

    created kafka loading bat file

--- producer_finnhub/wt_load.bat
+++ producer_finnhub/wt_load.bat
@@ -1,4 +1,17 @@
-wt.exe -w 1 nt PowerShell --title ZooKeep --tabcolor "#F00"  -d C:/personal.programs/kafka -c .\bin\windows\zookeeper-server-start.bat .\config\zookeeper.properties
-#wt.exe -w 1 nt PowerShell --title Kafka   --tabcolor "#0F0" --startingDirectory
-#wt.exe -w 1 nt PowerShell --title Prodcer --tabcolor "#00F" --startingDirectory
-#wt.exe -w 1 nt PowerShell --title Consmer --tabcolor "#F0F" --startingDirectory
+# start zookeeper
+wt -w 0 nt --startingDirectory c:/personal.programs/kafka --title ZooKeper --tabColor "#8AD8FF" --colorScheme "One Half Dark" powershell -noExit ".\bin\windows\zookeeper-server-start.bat .\config\zookeeper.properties"
+
+# start kafka server
+wt -w 0 nt --startingDirectory c:/personal.programs/kafka --title KafkaServer --tabColor "#F0F" --colorScheme "One Half Dark" powershell -noExit ".\bin\windows\kafka-server-start.bat .\config\server.properties"
+
+# start producer for existing topic topic_finnhub 
+wt -w 0 nt --startingDirectory c:/personal.programs/kafka --title Producer --tabColor "#00F" --colorScheme "One Half Dark" powershell -noExit ".\bin\windows\kafka-console-producer.bat --topic topic_finnhub --bootstrap-server localhost:9092"
+
+# start Consumer for existing topic topic_finnhub 
+wt -w 0 nt --startingDirectory c:/personal.programs/kafka --title Consumer --tabColor "#0F0" --colorScheme "One Half Dark" powershell -noExit ".\bin\windows\kafka-console-consumer.bat --topic topic_finnhub --bootstrap-server localhost:9092 --from-beginning"
+
+
+
+
+
+
