81320ae8a7456c5a107612e2a8a790c828c04979
Akintunde Adegbayo <akt231@yahoo.com>
2023-11-12 05:33:05 +0100

    docker compose file work

--- kafka/scripts/kafka_create_topic.bat
+++ kafka/scripts/kafka_create_topic.bat
@@ -5,7 +5,7 @@ echo -e 'List kafka topics'
 %kafka_dir%/kafka-topics.bat --bootstrap-server localhost:29092 --list
 
 echo -e 'Creating kafka topics'
-%kafka_dir%/kafka-topics.bat --bootstrap-server localhost:29092 --create --if-not-exists --topic topic_finnhub --replication-factor 1 --partitions 1
+%kafka_dir%/kafka-topics.bat --bootstrap-server localhost:29092 --create --if-not-exists --topic trades --replication-factor 1 --partitions 1
 
 echo -e 'Successfully created the following topics:'
 %kafka_dir%/kafka-topics.bat --bootstrap-server localhost:29092 --listP
--- kafka/scripts/kafka_init_server.bat
+++ kafka/scripts/kafka_init_server.bat
@@ -6,12 +6,12 @@ timeout 5
 wt -w 0 nt --startingDirectory c:/personal.programs/kafka --title KafkaServer --tabColor "#F0F" --colorScheme "One Half Dark" powershell -noExit ".\bin\windows\kafka-server-start.bat .\config\server.properties"
 timeout 5
 
-# start producer for existing topic topic_finnhub 
-wt -w 0 nt --startingDirectory c:/personal.programs/kafka --title Producer --tabColor "#00F" --colorScheme "One Half Dark" powershell -noExit ".\bin\windows\kafka-console-producer.bat --topic topic_finnhub --bootstrap-server localhost:9092"
+# start producer for existing topic trades 
+wt -w 0 nt --startingDirectory c:/personal.programs/kafka --title Producer --tabColor "#00F" --colorScheme "One Half Dark" powershell -noExit ".\bin\windows\kafka-console-producer.bat --topic trades --bootstrap-server localhost:9092"
 timeout 5
 
-# start Consumer for existing topic topic_finnhub 
-wt -w 0 nt --startingDirectory c:/personal.programs/kafka --title Consumer --tabColor "#0F0" --colorScheme "One Half Dark" powershell -noExit ".\bin\windows\kafka-console-consumer.bat --topic topic_finnhub --bootstrap-server localhost:9092 --from-beginning"
+# start Consumer for existing topic trades 
+wt -w 0 nt --startingDirectory c:/personal.programs/kafka --title Consumer --tabColor "#0F0" --colorScheme "One Half Dark" powershell -noExit ".\bin\windows\kafka-console-consumer.bat --topic trades --bootstrap-server localhost:9092 --from-beginning"
 
 
 
